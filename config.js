ac43510048eac432032f92c57f99fbd0720c26fc
Roy Baron <baron.roy@gmail.com>
2018-02-23 13:59:02 -0500

    set up basic server scaffolding

---
+++ config.js
@@ -0,0 +1,9 @@
+exports.DATABASE_URL =
+  process.env.DATABASE_URL ||
+  global.DATABASE_URL ||
+  `mongodb://localhost/myAuthDB`;
+
+exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET || "pizzapizzabananabanana";
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || `7d`;
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000";
