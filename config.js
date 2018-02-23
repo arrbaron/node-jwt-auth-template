67407051700d20afabcbdcbc3b7d0688027fb4e9
Roy Baron <baron.roy@gmail.com>
2018-02-23 14:26:48 -0500

    add endpoints for register and login

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ exports.DATABASE_URL =
   `mongodb://localhost/myAuthDB`;
 
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET || "pizzapizzabananabanana";
+exports.JWT_SECRET = process.env.JWT_SECRET || 'pizzapizzabananabanana';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || `7d`;
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000";
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
---
+++ users/router.js
@@ -0,0 +1,55 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { User } = require('./model');
+
+const router = express.Router();
+
+const jsonParser = bodyParser.json();
+
+// Post to register a new user
+router.post('/', jsonParser, (req, res) => {
+  let { username, password, firstName = '', lastName = '' } = req.body;
+  // Username and password come in pre-trimmed, otherwise we throw an error
+  // before this
+  firstName = firstName.trim();
+  lastName = lastName.trim();
+
+  return User.find({ username })
+    .count()
+    .then(count => {
+      if (count > 0) {
+        // There is an existing user with the same username
+        return Promise.reject({
+          code: 422,
+          reason: 'ValidationError',
+          message: 'Username already taken',
+          location: 'username'
+        });
+      }
+      // If there is no existing user, hash the password
+      return User.hashPassword(password);
+    })
+    .then(hash => {
+      return User.create({
+        username,
+        password: hash,
+        firstName,
+        lastName
+      });
+    })
+    .then(user => {
+      return res.status(201).json(user.serialize());
+    })
+    .catch(err => {
+      // Forward validation errors on to the client, otherwise give a 500
+      // error because something unexpected has happened
+      if (err.reason === 'ValidationError') {
+        return res.status(err.code).json(err);
+      }
+      res.status(500).json({ code: 500, message: 'Internal server error' });
+    });
+});
+
+module.exports = { router };
